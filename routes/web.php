1b21d827ac1692fd7cca25b744cedf4da4ae1029
nangzy <127278664+nangzy@users.noreply.github.com>
2023-05-05 21:15:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-
-// Route::get('/1', function () {
-//     return view('home',[
-//         'id' => 1,
-//         'user' => User::all()
-//     ]);
-// });
-
-// Route::get('/2', function () {
-//     return view('home',[
-//         'id' => 2,
-//         'user' => User::all()
-//     ]);
-// });
-
 Route::get('/{users:id}',[Controller::class, 'index']);
 Route::get('/coffee/{users:id}',[CoffeeController::class, 'index']);
 Route::post('/coffee/buy',[CoffeeController::class, 'buy'])->name('coffee.buy');
